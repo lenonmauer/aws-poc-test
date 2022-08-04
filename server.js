908af14c17915f4e745b796f09e9097500df4db6
Lenon Mauer <lenonmauer0@gmail.com>
2022-08-03 22:44:07 -0300

    added custom server

---
+++ server.js
@@ -0,0 +1,26 @@
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = 3000;
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    const { pathname, query } = parsedUrl;
+
+    console.log("pathname", pathname);
+    console.log("headers", req.headers);
+    console.log("query", query);
+
+    await handle(req, res, parsedUrl);
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://${hostname}:${port}`);
+  });
+});
