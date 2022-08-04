051f07c8e3d3fbcdf502288e428b3407b642c4f4
Lenon Mauer <lenonmauer0@gmail.com>
2022-08-03 22:47:53 -0300

    fixed host

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { parse } = require("url");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
+const hostname = process.env.HOST || process.env.IP || "0.0.0.0";
 const port = process.env.PORT || 3000;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
